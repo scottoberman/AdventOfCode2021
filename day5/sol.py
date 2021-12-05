339f85f33557442bf6b62527fc7282b5f7b4647c
Scott Oberman <scottoberman@gmail.com>
2021-12-04 22:10:10 -0800

    day5 part2 done

--- day5/sol.py
+++ day5/sol.py
@@ -17,25 +17,28 @@ def ventStuff(lines):
         y1 = int(line[1])
         y2 = int(line[3])
 
+        incX = 1
+        incY = 1
+        if x1 > x2:
+            incX = -1
+        if y1 > y2:
+            incY = -1
 
-        # Horiz or Vertical
+        # Horizontal and Vertical
         if (x1 == x2 and y1 != y2) or (x1 != x2 and y1 == y2):
-            incX = 1
-            incY = 1
-
-            if x1 > x2:
-                incX = -1
-            if y1 > y2:
-                incY = -1
-
             for x in range(x1, x2 + incX, incX):
                 for y in range(y1, y2 + incY, incY):
                     coordTemp = (x,y)
                     daVents[coordTemp] += 1
-        #else:
-            # Don't allow diags
-            
-                
+
+        # Diagonals
+        elif (abs(x2 - x1) == abs(y2 - y1)):
+            y = y1
+            for x in range(x1, x2 + incX, incX):
+                coordTemp = (x,y)
+                y += incY
+                daVents[coordTemp] += 1
+  
     coords = [x.replace(",", " ").replace(" ->", "").split() for x in lines]
     daVents = Counter()
     
@@ -49,14 +52,12 @@ def ventStuff(lines):
 
     return dubs
 
-
-
-
-    
 def daGoods():
+    # Part 1 example == 5
     exinput = readInputFile("exinput.txt")
     print(ventStuff(exinput))
 
+    # Part 1 == 6005
     input = readInputFile("input.txt")
     print(ventStuff(input))
 
