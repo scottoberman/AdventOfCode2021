9a6eb8a22d17d65ceba5a92005c1e84b303569bf
Scott Oberman <scottoberman@gmail.com>
2021-12-08 02:27:58 -0800

    part2 progress

--- day8/sol.py
+++ day8/sol.py
@@ -16,9 +16,27 @@ def Count1478(lines):
     # General formation rules:
     #  * 1 -> 7 -> 4 -> (2, 3, 5)  -> (0, 6, 9) -> 8
     # EZ Digits: 1, 7, 4, 8
+    def charMatchCount(chars1: set, chars2: set):
 
-    insAndOuts = []
-    storage1478 = []
+        if len(chars1) >= len(chars2):
+            source = chars1
+            target = chars2
+        else:
+            source = chars2
+            target = chars2
+
+        matchCount = 0
+        for char in source:
+            if char in target:
+                matchCount += 1
+                
+        return matchCount
+            
+
+    ins = []
+    outs = []
+    outsResolved = []
+    outsTotal = []
 
     len1 = 2
     len7 = 3
@@ -31,73 +49,58 @@ def Count1478(lines):
     chars1 = {}
     numStuff = {}
 
-    # Get the non overlapping char between two strings
-    def Get1NonOverlap(strLong, strShort):
-        strPrcs = str(strLong)
-        for x in strLong:
-            if x in strShort:
-                strPrcs.replace(x, '')
-        
-        assert(len(strPrcs) == 1)
-        return strPrcs[0]
-    
-    
 
     # Parse Input
     for line in lines:
-        insAndOuts.append((line.split(' | ')[0].split(), line.split(' | ')[1].split()))
+        ins = line.split(' | ')[0].split()
+        outs = line.split(' | ')[1].split()
 
-    for entry in insAndOuts:
-        for inSig in entry[0]:
-            match len(inSig):
-                case 1:
-                    chars1[1] = (set([char for char in inSig]))
-                case 7:
-                    chars1[7] = (set([char for char in inSig]))
+    for entry in ins:
+            match len(entry):
+                case 2:
+                    chars1[1] = (set([char for char in entry]))
+                case 3:
+                    chars1[7] = (set([char for char in entry]))
                 case 4:
-                    chars1[4] = (set([char for char in inSig]))
-                case 8:
-                    chars1[8] = (set([char for char in inSig]))
+                    chars1[4] = (set([char for char in entry]))
+                case 7:
+                    chars1[8] = (set([char for char in entry]))
 
-    chars1[9].update(chars1[4])
-    chars1[9].update(chars1[7])
-
-    chars1[3].update(chars1[7])
-
-    chars1[0].update(chars1[7])
-
-    # 1   1   1
-    # 2       3
-    # 2       3
-    # 2       3
-    # 4   4   4
-    # 5       6
-    # 5       6
-    # 5       6
-    # 7   7   7
-    if chars1.has_key(1):
-        if chars1.has_key(7):
-            numStuff[1] = Get1NonOverlap(chars1[7], chars1[1])
-    elif chars1.has_key(8):
-        if chars1.has_key(9):
-            numStuff[5] = Get1NonOverlap(chars1[8], chars1[9])
-        if chars1.has_key(0):
-            numStuff[4] = Get1NonOverlap(chars1[8], chars1[0])
-    elif chars1.has_key(6):
-        if chars1.has_key(5):
-            numStuff[5] = Get1NonOverlap(chars1[6], chars1[5])
-        if chars1.has_key(8):
-            numStuff[3] = Get1NonOverlap(chars1[8], chars1[6])
-        if chars1.has_key(7):
-            numStuff[3] = Get1NonOverlap(chars1[6], chars1[7])
-    elif chars1.has_key(5):
-        if chars1.has_key(7):
-            numStuff[3] = Get1NonOverlap(chars1[5], chars1[7])
-    
-
+    for entry in outs:
+        if charMatchCount(chars1[8], set(entry)) == 6:
+            # 9
+            if charMatchCount(chars1[1], set(entry)) == 2:
+                outsResolved.append('9')
+            # 6
+            elif charMatchCount(chars1[1], set(entry)) == 1:
+                outsResolved.append('6')
+            # 0
+            elif charMatchCount(chars1[4], set(entry)) == 3:
+                outsResolved.append('0')
+        # 3
+        elif charMatchCount(chars1[7], set(entry)) == 3:
+            outsResolved.append('3')
+        # 2
+        elif charMatchCount(chars1[4], set(entry)) == 2:
+            outsResolved.append('2')
+        # 5
+        elif charMatchCount(chars1[4], set(entry)) == 3:
+            outsResolved.append('5')
+        elif len(entry) == len1:
+            outsResolved.append('1')
+        elif len(entry) == len7:
+            outsResolved.append('7')
+        elif len(entry) == len4:
+            outsResolved.append('4')
+        elif len(entry) == len8:
+            outsResolved.append('8')
+        else:
+            assert(False)
         
+    outsResolved = outsResolved.join()
+    outsTotal.append(outsResolved)
 
-    return len(storage1478)
+    return outsResolved
     
                 
                     
