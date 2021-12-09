249dbb5823da551788bb5b78184b6f6ad092ae3f
Scott Oberman <scottoberman@gmail.com>
2021-12-09 00:17:11 -0800

    day9 part2 progress
    dfs stuff

--- day9/sol.py
+++ day9/sol.py
@@ -10,32 +10,59 @@ def readInputFile(fileName):
 
     return inputLines
 
+def DFSBasin(basinInd, pointsToCheck: set, grid, basinNum, basinData: dict):
+        lineIndex, eleIndex = basinInd.pop()
+        basinData[(lineIndex, eleIndex)] = basinNum
+        # Moving left
+        if (lineIndex, eleIndex - 1) in pointsToCheck and (eleIndex > 0  and grid[lineIndex][eleIndex - 1] != 9):
+            pointsToCheck.remove((lineIndex, eleIndex - 1))
+            basinInd.append((lineIndex, eleIndex - 1))
+            DFSBasin(basinInd, pointsToCheck, grid, basinNum, basinData)
+        # Moving right
+        if (lineIndex, eleIndex + 1) in pointsToCheck and (eleIndex < len(grid[0]) - 1  and grid[lineIndex][eleIndex + 1] != 9):
+            pointsToCheck.remove((lineIndex, eleIndex + 1))
+            basinInd.append((lineIndex, eleIndex + 1))
+            DFSBasin(basinInd, pointsToCheck, grid, basinNum, basinData)
+        # Moving down
+        if lineIndex > 0 and grid[lineIndex - 1][eleIndex] != 9:
+            pointsToCheck.remove((lineIndex - 1, eleIndex))
+            basinInd.append((lineIndex - 1, eleIndex))
+            DFSBasin(basinInd, pointsToCheck, grid, basinNum, basinData)
+        # Moving up
+        if lineIndex < len(grid) - 1 and grid[lineIndex + 1][eleIndex] != 9:
+            pointsToCheck.remove((lineIndex + 1, eleIndex))
+            basinInd.append((lineIndex + 1, eleIndex))
+            DFSBasin(basinInd, pointsToCheck, grid, basinNum, basinData)
+
+
 def GetLowPointsRiskScore(lines):
     grid = [[int(ele) for ele in line] for line in lines]
-    lowPoints = [] # Coordinates then val
 
-    for lineIndex, line in enumerate(grid):
-        for eleIndex, ele in enumerate(line):
-            lowLeft  = False
-            lowRight = False
-            lowUp    = False
-            lowDown  = False
-            if eleIndex == 0  or ele < grid[lineIndex][eleIndex - 1]:
-                lowLeft = True
-            if eleIndex == len(line) - 1  or ele < grid[lineIndex][eleIndex + 1]:
-                lowRight = True
-            if lineIndex == 0 or ele < grid[lineIndex - 1][eleIndex]:
-                lowUp = True
-            if lineIndex == len(grid) - 1 or ele < grid[lineIndex + 1][eleIndex]:
-                lowDown = True
-            
-            if lowLeft and lowRight and lowUp and lowDown:
-                lowPoints.append(((lineIndex, eleIndex), ele))
+    basinData = []
+    basinInd = []
 
-    riskScore = 0
-    for ele in lowPoints:
-        riskScore += ele[1] + 1
-    return riskScore
+    pointsToCheck = set()
+    basinData = dict() # Coord then basin number
+    coordFirst = 0
+    getFirstPoint = True
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] != 9:
+                pointsToCheck.add((x,y))
+                if getFirstPoint:
+                    # Pretty random start point for checking basins
+                    basinInd = [(x,y)]
+                    coordFirst = (x,y)
+                    getFirstPoint = False
+
+    basinNum = 0
+
+    pointsToCheck.remove(coordFirst)
+    # Have to jump between basins.
+    while len(pointsToCheck) > 0:
+        DFSBasin(basinInd, pointsToCheck, grid, basinNum, basinData)
+        basinNum += 1
 
 def daGoods():
     print(GetLowPointsRiskScore(readInputFile("exinput.txt")))
