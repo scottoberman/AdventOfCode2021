de03f0ba4c88f6cf28f9f4ea21b687593ba04076
Scott Oberman <scottoberman@gmail.com>
2021-12-18 08:17:48 -0800

    day15 part1 done

--- day15/sol.py
+++ day15/sol.py
@@ -2,63 +2,69 @@ import os
 import heapq
 from collections import Counter, OrderedDict
 
-charFill = '.'
-charMark = '#'
-
 def readInputFile(fileName):
     __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__))) + '\\' + fileName
     inputFile = open(__location__, 'r')
     inputLines = inputFile.readlines()
     inputLines = [x.strip('\n') for x in inputLines ]
+    
+    return inputLines
 
+class Node:
+    def __init__(self, coord: tuple, value, prev) -> None:
+        self.coord = coord
+        self.value = value
+        self.prev = prev
+
+    def __lt__(self, rhs):
+        return self.value < rhs.value
 class DaGame:
-    class DaPath:
-        def __init__(self, nodes = []) -> None:
-            self.score = 0
-            self.nodes = list(nodes)
-        def addNode(self, node, score):
-            self.nodes.append((score, node))
-            self.score += score
-        def pop(self):
-            nodeRemoved = self.nodes.pop()
-            self.score -= nodeRemoved[0]
-            
     def __init__(self, lines) -> None:
+        self.board = []
         for indexLine, line in enumerate(lines):
+            self.board.append([])
             for indexRisk, risk in enumerate(line):
-                self.board[indexLine][indexRisk] = risk
+                self.board[indexLine].append(int(risk))
 
-    def __isInRange((x,y)):
-        return x < len(self.board) and y < len(self.board[0])
+    def __isInRange(self, coord):
+        return coord[0] < len(self.board) and coord[1] < len(self.board[0])
 
     def PlayGame(self):
-        pathCur = DaGame.DaPath()
-        nodesExplored = set()
+        coordsExplored = set()
+        coordsToVisit = set()                
+        nodesPotential = []
 
-        self.Search(pathCur, nodesExplored)
+        for x in range(len(self.board)):
+            for y in range(len(self.board[x])):
+                if x != 0 or y != 0:
+                    coordsToVisit.add((x,y))
+                    heapq.heappush(nodesPotential, Node((x,y), 9999999, None))
 
-    def Search(self, pathCur, nodesExplored):
-        for xOff in range(-1, 2):
-            for yOff in range(-1, 2):
-                nodeNew = (xOff,yOff) + nodeCur
-                if (xOff == 0 or yOff == 0) and\
-                (abs(xOff) != abs(yOff)) and \
-                (xOff,yOff) not in nodesExplored and \
-                self.__isInRange(nodeNew):
-                    nodeCur = pathCur.nodes[-1][-1]
-                    
-                    pathCur.addNode(nodeNew, self.board[x][y])
-                    # Node is encountered 1st on the shortest
-                    # path.
-                    nodesExplored.add(nodeNew)
-                    if nodeNew[0] == len(self.board) - 1 and nodeNew[0][0] == len(self.board[0]) - 1:
-                        print(pathCur.score)
-                    else:
-                        pathCur.pop()
+        nodeCur = Node((0, 0), 0, None)
+        heapq.heappush(nodesPotential, nodeCur)
+
+        while True:
+            for xOff in range(-1, 2):
+                for yOff in range(-1, 2):
+                    coordNew = (xOff + nodeCur.coord[0], yOff + nodeCur.coord[1])
+                    if (xOff == 0 or yOff == 0) and\
+                    (abs(xOff) != abs(yOff)) and \
+                    coordNew in coordsToVisit and \
+                    self.__isInRange(coordNew):
+                        nodeNew = Node(coordNew, nodeCur.value + self.board[coordNew[0]][coordNew[1]], nodeCur)   
+                        coordsToVisit.remove(coordNew)
+                        heapq.heappush(nodesPotential, nodeNew) # Gonna have a bunch of duplicates currently
+                        if coordNew[0] == len(self.board) - 1 and coordNew[1] == len(self.board[0]) - 1:
+                            print (nodeNew.value)
+            nodeCur = heapq.heappop(nodesPotential)
 
             
-        
+def dieGuter():
+    # ex = DaGame(readInputFile("exinput.txt"))
+    # ex.PlayGame()
 
-        
+    actual = DaGame(readInputFile("input.txt"))
+    actual.PlayGame()
 
+dieGuter()
