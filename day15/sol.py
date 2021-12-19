5da39ea469e3529b7445edaf2de1e3ce754d6f27
Scott Oberman <scottoberman@gmail.com>
2021-12-18 21:37:29 -0800

    day15 part2 done

--- day15/sol.py
+++ day15/sol.py
@@ -26,6 +26,30 @@ class DaGame:
             self.board.append([])
             for indexRisk, risk in enumerate(line):
                 self.board[indexLine].append(int(risk))
+        
+        self.__Part2Board(5)
+    
+    def __Part2Board(self, n):
+        boardNew = [row[:] for row in self.board]
+        # Generate first row with n duplicate first blocks
+        for x in range(len(self.board)):
+            for y in range(0, len(self.board[x]) - 1 + (n - 2) * (len(self.board[x])) + 1):
+                boardNew[x].append(self.__incRisk(boardNew[x][y]))
+        
+        # Duplicate first row downwards
+        for x in range(0, len(self.board) - 1 + (n - 2) * (len(self.board)) + 1):
+            boardNew.append([])
+            for y in range(len(boardNew[0])):
+                boardNew[-1].append(self.__incRisk(boardNew[x][y]))
+
+        self.board = boardNew
+        
+
+    def __incRisk(self, risk):
+        if risk > 8:
+            return 1
+        else:
+            return risk + 1
 
     def __isInRange(self, coord):
         return coord[0] < len(self.board) and coord[1] < len(self.board[0])
@@ -56,15 +80,15 @@ class DaGame:
                         coordsToVisit.remove(coordNew)
                         heapq.heappush(nodesPotential, nodeNew) # Gonna have a bunch of duplicates currently
                         if coordNew[0] == len(self.board) - 1 and coordNew[1] == len(self.board[0]) - 1:
-                            print (nodeNew.value)
+                            return (nodeNew.value)
             nodeCur = heapq.heappop(nodesPotential)
 
             
 def dieGuter():
-    # ex = DaGame(readInputFile("exinput.txt"))
-    # ex.PlayGame()
+    ex = DaGame(readInputFile("exinput.txt"))
+    print(ex.PlayGame())
 
     actual = DaGame(readInputFile("input.txt"))
-    actual.PlayGame()
+    print(actual.PlayGame())
 
 dieGuter()
